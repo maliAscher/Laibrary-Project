f10ccda3d3d0e64a42632ab2f0a93537e8dcf772
maliAscher <malistar839@gmail.com>
2024-07-01 00:43:53 +0300

    Add Routing to the project

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,15 @@
 // src/App.js
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Book from './components/Book';
+import Demo from './components/Demo';
 
 const App = () => {
   return (
-    <div className="App">
-      <Book />
-    </div>
+    <Routes>
+      <Route path='/' element={<Book />} />
+      <Route path='/demo' element={<Demo />} />
+    </Routes>
   );
 };
 
---
+++ Client/src/components/Demo.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Demo() {
+  return (
+    <h1>Hello and welcome!!!!</h1>
+  )
+}
